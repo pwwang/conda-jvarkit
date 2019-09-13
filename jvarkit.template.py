b162c8584b05aa8e2e409fb87ea10446c7b99206
pwwang <pwwang@pwwang.com>
2019-09-13 00:24:27 -0500

    Finish most functions

--- jvarkit.template.py
+++ jvarkit.template.py
@@ -16,23 +16,15 @@ def try_import(package):
 	finally:
 		return __import__(package)
 
-cmdy     = try_import('cmd')
-commands = getattr(try_import('pyparam'), 'commands')
+cmdy        = try_import('cmdy')
+commands    = getattr(try_import('pyparam'), 'commands')
 
 JARDIR = Path('{{jardir}}')
+# for development
+if '{{' in str(JARDIR) and '}}' in str(JARDIR):
+	JARDIR = Path(environ.get('JVARKIT_JARDIR', ''))
 
-commands._.version          = False
-commands._.version.desc     = 'Show current version of jvarkit.'
-commands._.jardir           = ''
-commands._.jardir.show      = False
-commands._.jardir.desc      = 'The directory containing jar files for jvarkit tools, only used in development mode.'
-commands.install            = 'Install a jvarkit tool.'
-commands.install._.required = True
-commands.install._.desc     = 'The jvarkit tool to be installed. Use "jvarkit list" to see all available tools.'
-commands.list               = 'List available jvarkit tools.'
-commands.list._hbald        = False
-commands.version            = 'Show current version of jvarkit.'
-
+@lru_cache()
 def is_installed(tool):
 	return (JARDIR / (tool + '.jar')).exists()
 
@@ -47,6 +39,79 @@ def get_tools():
 			ret[tool] = tr.find('td').text
 	return ret
 
+@lru_cache()
+def tool_help(tool, java = 'java'):
+	return str(cmdy.java('-jar', str(JARDIR / (tool + '.jar')), h = True, _exe = java))
+
+def helpx(helps):
+	helps.delete('Global optional options')
+	helps.delete('Available commands')
+	helps.add('Available commands')
+	helps.select('Available commands').addCommand(commands.install, ['install']) \
+		.addCommand(commands.list, ['list']) \
+		.addCommand(commands.version, ['version']) \
+		.addCommand(commands.help, ['help'], ishelp = True)
+	sec = helps.add('Installed tools', sectype = 'option')['Installed tools']
+	for tool, desc in get_tools().items():
+		if is_installed(tool):
+			sec.add((tool, '', desc))
+
+def tool_helpx(tool):
+	def help_callback(helps):
+		helps.add('Java options', [
+			('-X../-D..', '', 'Java Virtual Machine Parameters')
+		])
+		req_sec = helps.add('Required tool options', sectype = 'options')['Required tool options']
+		opt_sec = helps.add('Optional tool options', sectype = 'options')['Optional tool options']
+		tool_help_lines = tool_help(tool).splitlines()
+		started = False
+		option = {'raw': None, 'req': False}
+		def addoption(option):
+			if not option['raw']:
+				return
+			(req_sec if option['req'] else opt_sec).add(tuple(option['raw']))
+			option['raw'], option['req'] = None, False
+
+		for line in tool_help_lines:
+			line = line.strip()
+			if line.startswith('Options'):
+				started = True
+				continue
+			if not started:
+				continue
+			if line.startswith('-'):
+				addoption(option)
+				option['raw'] = [line, '', []]
+			elif line.startswith('* -'):
+				addoption(option)
+				option['req'] = True
+				option['raw'] = [line[2:], '', []]
+			else:
+				option['raw'][2].append(line)
+		addoption(option)
+
+	return help_callback
+
+commands.install            = 'Install a jvarkit tool.'
+commands.install._.required = True
+commands.install._.desc     = ['The jvarkit tool to be installed.',
+							   'Use "jvarkit list" to see all available tools.',
+							   'Use "jvarkit install all" to install all tools.']
+commands.list               = 'List available jvarkit tools.'
+commands.list._hbald        = False
+commands.version            = 'Show current version of jvarkit.'
+
+for tool, desc in get_tools().items():
+	if is_installed(tool):
+		commands[tool] = str(desc)
+		commands[tool]._helpx = tool_helpx(tool)
+		commands[tool].hh = False
+		commands[tool].hh.desc = 'Show original help page.'
+		commands[tool].java = 'java'
+		commands[tool].java.desc = 'Path to java executable.'
+
+commands._helpx = helpx
+
 def list_tools():
 	print("")
 	print("INSTALLED | TOOL                       | DESCRIPTION")
@@ -59,15 +124,46 @@ def list_tools():
 def show_version():
 	print('jvarkit v%s built by conda-jvarkit.' % __version__)
 
-if __name__ == '__main__':
-	command, opts, gopts = commands._parse(arbi = True)
-	#print(command, opts, gopts)
-	if gopts['jardir']:
-		JARDIR = Path(gopts['jardir'])
-	if command == 'version' or gopts['version']:
-		show_version()
-	if command == 'list':
-		list_tools()
-	if command == 'vcfstats':
-		pass
+def install_tool(tool):
+	cmdy.gradlew(_fg = True, _exe = str(JARDIR.parent / 'gradlew'), p = str(JARDIR.parent), _ = tool)
 
+
+def run_tool(tool, opts):
+	for hopt in commands[tool]._hopts:
+		del opts[hopt]
+
+	java     = opts.pop('java')
+	hh       = opts.pop('hh')
+	javaopts = [pos for pos in opts.get('_', []) if pos[:2] in ('-X', '-D')]
+
+	opts['_'] = [pos for pos in opts.get('_', []) if pos[:2] not in ('-X', '-D')]
+	javaopts.append('-jar')
+	javaopts.append(str(JARDIR / (tool + '.jar')))
+	if hh:
+		print(tool_help(tool, java))
+		return
+	opts['_fg']  = True
+	opts['_exe'] = java
+	try:
+		cmdy.java(*javaopts, **opts)
+	except cmdy.CmdyReturnCodeException:
+		commands[tool]._help(print_and_exit = True)
+
+if __name__ == '__main__':
+
+	command, opts, _ = commands._parse(arbi = True)
+
+	if command == 'help':
+		commands._help(print_and_exit = True)
+	elif command == 'install':
+		install_tool(opts['_'])
+	elif command == 'version':
+		show_version()
+	elif command == 'list':
+		list_tools()
+	elif command not in get_tools():
+		commands._help(error = 'No such command/tool: %s' % command, print_and_exit = True)
+	elif not is_installed(command):
+		print('Tool "%s" is not installed, try "jvarkit install %s" to install it' % (command, command))
+	else:
+		run_tool(command, opts)
